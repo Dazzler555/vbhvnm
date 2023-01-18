1c831f9919731b2ee8bb38ef938ab13939c9196e
Dazzler555 <71560605+Dazzler555@users.noreply.github.com>
2023-01-19 03:37:34 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const request = require("request");
+const port = process.env.PORT || 5000
+const app = express();
+const url = process.env.URL; // Use the global environment variable for the URL
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+let nextPing = new Date();
+
+// Function to ping the URL
+function pingUrl() {
+    request(url, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            console.log("Successfully pinged: " + url);
+            nextPing = new Date(Date.now() + 60000);
+        } else {
+            console.log("Error pinging: " + url);
+        }
+    });
+}
+
+// Set a timer to ping the URL every minute
+setInterval(pingUrl, 60000);
+
+app.get("/", (req, res) => {
+    res.render("index", { nextPing });
+});
+
+app.listen(port, () => {
+    console.log("Server started");
+});
