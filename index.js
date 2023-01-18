d117075c6481d95663bf21b22248790197fa5402
Dazzler555 <71560605+Dazzler555@users.noreply.github.com>
2023-01-19 04:13:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ function pingUrl() {
     request(url, (error, response, body) => {
         if (!error && response.statusCode === 200) {
             console.log("Successfully pinged: " + url);
-            nextPing = new Date(Date.now() + 60000);
+            if(nextPing < new Date()){
+                nextPing = new Date(Date.now() + 60000);
+            }
         } else {
             console.log("Error pinging: " + url);
         }
@@ -26,7 +28,41 @@ function pingUrl() {
 setInterval(pingUrl, 60000);
 
 app.get("/", (req, res) => {
-    res.render("index", { nextPing });
+    const html = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>URL Pinger</title>
+        </head>
+        <body>
+            <h1>Next ping in:</h1>
+            <p id="countdown"></p>
+            <script>
+                // Get the countdown element
+                const countdown = document.getElementById("countdown");
+
+                // Get the next ping time from the server
+                const nextPing = new Date("<%= nextPing %>");
+
+                // Update the countdown every second
+                setInterval(() => {
+                    // Get the current time
+                    const now = new Date();
+
+                    let timeUntilPing;
+                    if (now < nextPing) {
+                        timeUntilPing = nextPing - now;
+                    } else {
+                        timeUntilPing = now - nextPing;
+                    }
+                    // Update the countdown element
+                    countdown.innerHTML = (timeUntilPing / 1000).toFixed(3) + " seconds";
+                }, 1000);
+            </script>
+        </body>
+        </html>
+    `;
+    res.send(ejs.render(html, { nextPing }));
 });
 
 app.listen(3000, () => {
